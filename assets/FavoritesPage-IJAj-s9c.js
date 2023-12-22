d60425dcf438678ff0a8d727dfc5d0bcb4ed7e15
Camiilo <camiilo@MacBook-Air-de-Camilo.local>
2023-12-22 17:43:08 -0500

    Updates

---
+++ assets/FavoritesPage-IJAj-s9c.js
@@ -0,0 +1,13 @@
+import{r as l,u as m,f as x,b as g,j as e,d as p}from"./index-Lr_KbuvF.js";import{S as f,s as w}from"./search-4rGf7a0u.js";import{n as r}from"./emotion-styled.browser.esm-aCa4HUhZ.js";function v(){const[i,a]=l.useState(!1),t=m(x),o=g();l.useEffect(()=>{(()=>{(t==null?void 0:t.length)==0?a(!1):a(!0)})()},[t]);const c=s=>{o(p(s))},n=r.p`
+        color: white;
+        font-weight: bold;
+        font-size: 2rem;
+    `,d=r.p`
+        color: white;
+        font-size: 1rem;
+        font-weight: bold;
+    `,h=r.p`
+        color: white;
+        font-size: 0.6rem;
+        font-weight: 300;
+    `;return e.jsx(e.Fragment,{children:e.jsxs("section",{className:"h-screen w-screen",children:[e.jsx(f,{}),i?t.length>0&&e.jsx("div",{className:"mt-6 grid px-5 grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8",children:t.map(s=>e.jsxs("div",{className:"group relative max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700",children:[e.jsx("div",{className:"aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80",children:e.jsx("img",{src:`https://image.tmdb.org/t/p/w500/${s.poster_path}`,alt:s.title,className:"h-full w-full object-cover object-center lg:h-full lg:w-full"})}),e.jsxs("div",{className:"mt-4",children:[e.jsxs("div",{className:"flex",children:[e.jsx("img",{src:w,className:"logo w-5 text-white",alt:"start logo"}),e.jsx("span",{className:"text-xs py-3 mt-1 ml-1 text-gray-900 dark:text-white",children:s.popularity})]}),e.jsx(d,{className:"text-xs text-center",children:s.title}),e.jsx(h,{className:"mt-5",children:s.overview})]}),e.jsx("div",{className:"flex flex-col items-center pb-10",children:e.jsx("div",{className:"flex mt-4 md:mt-6",children:e.jsx("a",{onClick:()=>c(s.id),className:"inline-flex items-center px-5  text-white bg-red-700 px-5 rounded bg-border-red-700",children:"Eliminar"})})})]},s.id))}):e.jsx(n,{className:"m-5",children:"Sin favoritos..."})]})})}export{v as default};
---
+++ assets/NotFound-PomRDSv4.js
@@ -0,0 +1,7 @@
+import{j as e,L as o}from"./index-Lr_KbuvF.js";import{n as s}from"./emotion-styled.browser.esm-aCa4HUhZ.js";function a(){const n=s.h1`
+    color: red;
+    font-weight: 100;
+  `,t=s.p`
+    color: white;
+    font-weight: bold;
+  `;return e.jsx(e.Fragment,{children:e.jsx("section",{className:"flex h-screen w-screen flex-1 flex-col justify-center px-6 py-12 lg:px-",children:e.jsxs("div",{className:"text-center",children:[e.jsx("blockquote",{className:"text-gray-900",children:e.jsx(n,{children:"404 - No Encontrado"})}),e.jsx(t,{className:"text-sm md:text-base lg:text-lg mt-10",children:"¡Oops! La página que estás buscando no pudo ser encontrada."}),e.jsxs(t,{className:"text-sm md:text-base lg:text-lg mt-2",children:["Por favor, verifica la URL o regresa a la página de",e.jsx(o,{to:"/",className:"text-blue-500 hover:underline ml-1",children:"inicio"}),"."]})]})})})}export{a as default};
